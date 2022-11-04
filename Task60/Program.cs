41a2bf36e2cf6321e344f5bba1ce3d01763ebf86
Kate <vvellspring@gmail.com>
2022-11-04 17:49:13 +0300

    Задача 60, трехмерный массив - вывод индексов

---
+++ Task60/Program.cs
@@ -0,0 +1,36 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента. Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+Console.Clear();
+
+int[,,] FillArray(int num1, int num2, int num3)
+{
+    int[,,] matrix = new int[num1, num2, num3];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+    return matrix;
+}
+int[,,] myArray = FillArray(2,2,2);
+
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        for (int k = 0; k < myArray.GetLength(2); k++)
+        {
+            Console.WriteLine($"{myArray[i,j,k]} ({i},{j},{k})");
+        }
+    }
+}
